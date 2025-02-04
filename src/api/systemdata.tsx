9ac60cdf0f792082a5347c31d7fff922a74f53e0
Alec col <a.colq.c@hotmail.com>
2025-02-04 16:01:10 -0400

    added data to file systemdata

--- src/api/systemdata.tsx
+++ src/api/systemdata.tsx
@@ -98,3 +98,221 @@ export const categories = [
         icon: "fa-solid fa-circle-dot"
     }
 ]
+// data for subcategories
+export const subcategories = [
+    {
+        id: 1,
+        category_id: 1,
+        name: "subcategory 1",
+        slug: "subcategory-1",
+    },
+    {
+        id: 2,
+        category_id: 1,
+        name: "subcategory 2",
+        slug: "subcategory-2",
+    },
+    {
+        id: 3,
+        category_id: 1,
+        name: "subcategory 3",
+        slug: "subcategory-3",
+    },
+    {
+        id: 4,
+        category_id: 1,
+        name: "subcategory 4",
+        slug: "subcategory-4",
+    },
+]
+// data for customers
+export const customers = [
+    {
+        id: 1,
+        name: "client 1",
+        email: "client1@client.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci: 35674567
+    },
+    {
+        id: 2,
+        name: "client 2",
+        email: "client2@client.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci: 35674567
+    },
+    {
+        id: 3,
+        name: "client 3",
+        email: "client3@client.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci: 35674567
+    },
+    {
+        id: 4,
+        name: "client 4",
+        email: "client4@client.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci: 35674567
+    }
+]
+
+// data for suppliers
+export const suppliers = [
+    {
+        id: 1,
+        name: "juan perez",
+        email: "juan@juan.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci_nit: 35674567
+    },
+    {
+        id: 2,
+        name: "carlos perez",
+        email: "carlos@carlos.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci_nit: 35674567
+    },
+    {
+        id: 3,
+        name: "pedro perez",
+        email: "pedro@pedro.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci_nit: 35674567
+    },
+    {
+        id: 4,
+        name: "alicia perez",
+        email: "alicia@alicia.com",
+        phone: "+591 60427039",
+        address: "calle sucre, #45",
+        ci_nit: 35674567
+    }
+]
+// data for products
+export const products = [
+    {
+        id: 1,
+        name: "product 1",
+        slug: "product-1",
+        price: 100,
+        stock: 100,
+        description: "product 1 description",
+        image: "https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}"
+    },
+    {
+        id: 2,
+        name: "product 2",
+        slug: "product-2",
+        price: 100,
+        stock: 100,
+        description: "product 2 description",
+        image: "https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}"
+    },
+    {
+        id: 3,
+        name: "product 3",
+        slug: "product-3",
+        price: 100,
+        stock: 100,
+        description: "product 3 description",
+        image: "https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}"
+    },
+    {
+        id: 4,
+        name: "product 4",
+        slug: "product-4",
+        price: 100,
+        stock: 100,
+        description: "product 4 description",
+        image: "https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}"
+    },
+    {
+        id: 5,
+        name: "product 5",
+        slug: "product-5",
+        price: 100,
+        stock: 100,
+        description: "product 5 description",
+        image: "https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}"
+    }
+]
+// data for inventory
+export const inventory = [
+    {
+        id: 1,
+        product_id: 1,
+        shop_id: 1,
+        supplier_id: 1,
+        quantity: 100,
+        restock_date: new Date(),
+    },
+    {
+        id: 2,
+        product_id: 2,
+        shop_id: 2,
+        supplier_id: 2,
+        quantity: 100,
+        restock_date: new Date(),
+    },
+    {
+        id: 3,
+        product_id: 3,
+        shop_id: 3,
+        supplier_id: 3,
+        quantity: 100,
+        restock_date: new Date(),
+    },
+    {
+        id: 4,
+        product_id: 4,
+        shop_id: 4,
+        supplier_id: 4,
+        quantity: 100,
+        restock_date: new Date(),
+    }
+]
+// data for orders
+export const orders = [
+    {
+        id: 1,
+        customer_id: 1,
+        shop_id: 1,
+        quantity: 100,
+        total: 100,
+        status: "pending"
+    },
+    {
+        id: 2,
+        customer_id: 2,
+        shop_id: 2,
+        quantity: 100,
+        total: 100,
+        status: "pending"
+    },
+    {
+        id: 3,
+        customer_id: 3,
+        shop_id: 3,
+        quantity: 100,
+        total: 100,
+        status: "pending"
+    },
+    {
+        id: 4,
+        customer_id: 4,
+        shop_id: 4,
+        quantity: 100,
+        total: 100,
+        status: "pending"
+    }
+]
+
+
